05134ac592875a432ce14c856368f772d5078f47
anaaalle <anssi.lepikko@gmail.com>
2020-03-18 23:02:31 +0200

    Nimikkeet korjattu ja testattu, kun viimeksi jäi tekemättä. Pepe ja käyttöliittymäosuus ripulilla

--- pepe/src/fxPepe/Nimikkeet.java
+++ pepe/src/fxPepe/Nimikkeet.java
@@ -11,10 +11,10 @@ package fxPepe;
  */
 public class Nimikkeet {
     
-    private static final int MAX_PELEJA    = 5;
-    private int              lkm           = 0;
-    private String           tiedostonNimi = "";
-    private Peli             alkiot[]      = new Peli[MAX_PELEJA];
+    private static final int MAX_NIMIKKEITA     = 5;
+    private int              lkm                = 0;
+    private String           tiedostonNimi      = "";
+    private Nimike           alkiot[]           = new Nimike[MAX_NIMIKKEITA];
 
     /**
      * Oletusmuodostaja
@@ -25,43 +25,43 @@ public class Nimikkeet {
     
     
     /**
-     * Lisää uuden pelin tietorakenteeseen. Ottaa pelin omistukseensa.
-     * @param peli lisätäävän pelin viite. Huom tietorakenne muuttuu omistajaksi.
+     * Lisää uuden nimikkeen tietorakenteeseen. Ottaa nimikkeen omistukseensa.
+     * @param nimike Lisättävän nimikkeen viite. Huom tietorakenne muuttuu omistajaksi.
      * @throws SailoException jos tietorakenne on jo täynnä
      * @example
      * <pre name="test">
      * #THROWS SailoException 
-     * Pelit pelit = new Pelit();
-     * Peli cnc = new Peli(), ra = new Peli();
-     * pelit.getLkm() === 0;
-     * pelit.lisaa(cnc); pelit.getLkm() === 1;
-     * pelit.lisaa(ra); pelit.getLkm() === 2;
-     * pelit.lisaa(cnc); pelit.getLkm() === 3;
-     * pelit.anna(0) === cnc;
-     * pelit.anna(1) === ra;
-     * pelit.anna(2) === cnc;
-     * pelit.anna(1) == cnc === false;
-     * pelit.anna(1) == ra === true;
-     * pelit.anna(3) === cnc; #THROWS IndexOutOfBoundsException 
-     * pelit.lisaa(cnc); pelit.getLkm() === 4;
-     * pelit.lisaa(cnc); pelit.getLkm() === 5;
-     * pelit.lisaa(cnc);  #THROWS SailoException
+     * Nimikkeet nimikkeet = new Nimikkeet();
+     * Nimike cnc = new Nimike(), ra = new Nimike();
+     * nimikkeet.getLkm() === 0;
+     * nimikkeet.lisaa(cnc); nimikkeet.getLkm() === 1;
+     * nimikkeet.lisaa(ra); nimikkeet.getLkm() === 2;
+     * nimikkeet.lisaa(cnc); nimikkeet.getLkm() === 3;
+     * nimikkeet.anna(0) === cnc;
+     * nimikkeet.anna(1) === ra;
+     * nimikkeet.anna(2) === cnc;
+     * nimikkeet.anna(1) == cnc === false;
+     * nimikkeet.anna(1) == ra === true;
+     * nimikkeet.anna(3) === cnc; #THROWS IndexOutOfBoundsException 
+     * nimikkeet.lisaa(cnc); nimikkeet.getLkm() === 4;
+     * nimikkeet.lisaa(cnc); nimikkeet.getLkm() === 5;
+     * nimikkeet.lisaa(cnc);  #THROWS SailoException
      * </pre>
      */
-    public void lisaa(Peli peli) throws SailoException {
+    public void lisaa(Nimike nimike) throws SailoException {
         if (lkm >= alkiot.length) throw new SailoException("Liikaa alkioita");
-        alkiot[lkm] = peli;
+        alkiot[lkm] = nimike;
         lkm++;
     }
     
     
     /**
-     * Palauttaa viitteen i:teen peliin.
-     * @param i monennenko pelim viite halutaan
-     * @return viite peliim, jonka indeksi on i
+     * Palauttaa viitteen i:teen nimikkeeseen
+     * @param i monennenko nimikkeen viite halutaan
+     * @return viite nimikkeeseen, jonka indeksi on i
      * @throws IndexOutOfBoundsException jos i ei ole sallitulla alueella  
      */
-    public Peli anna(int i) throws IndexOutOfBoundsException {
+    public Nimike anna(int i) throws IndexOutOfBoundsException {
         if (i < 0 || lkm <= i)
             throw new IndexOutOfBoundsException("Laiton indeksi: " + i);
         return alkiot[i];
@@ -69,18 +69,18 @@ public class Nimikkeet {
     
     
     /**
-     * Lukee pelikannan tiedostosta. Kesken.
+     * Lukee nimikkeet tiedostosta. Kesken.
      * @param hakemisto tiedoston hakemisto
      * @throws SailoException jos lukeminen epäonnistuu
      */
     public void lueTiedostosta(String hakemisto) throws SailoException {
-        tiedostonNimi = hakemisto + "/pelit.dat";
+        tiedostonNimi = hakemisto + "/nimikkeet.dat";
         throw new SailoException("Ei osata vielä lukea tiedostoa " + tiedostonNimi);
     }
 
 
     /**
-     * Tallentaa pelikannan tiedostoon. Kesken.
+     * Tallentaa nimikkeet tiedostoon. Kesken.
      * @throws SailoException jos talletus epäonnistuu
      */
     public void talleta() throws SailoException {
@@ -89,7 +89,7 @@ public class Nimikkeet {
     
 
     /**
-     * Palauttaa pelirekisterin pelien lukumäärän
+     * Palauttaa nimikkeiden lukumäärän
      * @return jäsenten lukumäärä
      */
     public int getLkm() {
@@ -98,28 +98,27 @@ public class Nimikkeet {
 
     
     /**
-     * Testiohjelma pelikannalle
+     * Testiohjelma nimikkeille
      * @param args ei käytössä
      */
     public static void main(String args[]) {
-        Pelit pelit = new Pelit();
+        Nimikkeet nimikkeet = new Nimikkeet();
 
-        Peli d1 = new Peli(), d2 = new Peli();
+        Nimike d1 = new Nimike();
+        Nimike d2 = new Nimike();
         d1.rekisteroi();
-        d1.taytaPeliTiedoilla();
+        d1.taytaNimikeTiedoilla();
         d2.rekisteroi();
-        d2.taytaPeliTiedoilla();
+        d2.taytaNimikeTiedoilla();
 
         try {
-            pelit.lisaa(d1);
-            pelit.lisaa(d2);
+            nimikkeet.lisaa(d1);
+            nimikkeet.lisaa(d2);
 
-            System.out.println("============= Pelit testi =================");
-
-            for (int i = 0; i < pelit.getLkm(); i++) {
-                Peli peli = pelit.anna(i);
+            for (int i = 0; i < nimikkeet.getLkm(); i++) {
+                Nimike nimike = nimikkeet.anna(i);
                 System.out.println("Peli nro: " + i);
-                peli.tulosta(System.out);
+                nimike.tulosta(System.out);
             }
 
         } catch (SailoException ex) {
